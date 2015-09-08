a7f1f3e2077d36f2a7d2dc584787b141110b37a6
par.dahlman <par.dahlman@valtech.se>
2015-09-08 08:46:19 +0200

    Adding argument validation for preformatter

--- Common.Logging.Serilog.Tests/SerilogPreformatterTests.cs
+++ Common.Logging.Serilog.Tests/SerilogPreformatterTests.cs
@@ -6,11 +6,59 @@ namespace Common.Logging.Serilog.Tests
     public class SerilogPreformatterTests
     {
         private SerilogPreformatter _preformatter;
+        private string _resultTemplate;
+        private object[] _resultArgs;
 
         [SetUp]
         public void Setup()
         {
             _preformatter = new SerilogPreformatter();
+            _resultTemplate = string.Empty;
+            _resultArgs = null;
+        }
+
+        [Test]
+        public void Should_Return_Same_Template_String_If_Original_String_Is_Null_Or_Empty()
+        {
+            /* Setup */
+            const string originalTemplate = null;
+
+            /* Test */
+            var result = _preformatter.TryPreformat(originalTemplate, null, out _resultTemplate, out _resultArgs);
+
+            /* Assert */
+            Assert.That(result, Is.True);
+            Assert.That(_resultTemplate, Is.EqualTo(originalTemplate));
+        }
+
+        [Test]
+        public void Should_Return_Same_Template_String_If_No_Args_Provided()
+        {
+            /* Setup */
+            const string originalTemplate = "This is a template without any args";
+            
+            /* Test */
+            var result = _preformatter.TryPreformat(originalTemplate, null, out _resultTemplate, out _resultArgs);
+
+            /* Assert */
+            Assert.That(result, Is.True);
+            Assert.That(_resultTemplate, Is.EqualTo(originalTemplate));
+        }
+
+        [Test]
+        public void Should_Format_Entire_String_If_Only_Numeric_Formatting_Is_Used()
+        {
+            /* Setup */
+            const string originalTemplate = "Look at this,a {0} formatted string!";
+            var args = new object[] {"nicely "};
+            var expected = string.Format(originalTemplate, args);
+
+            /* Test */
+            var result = _preformatter.TryPreformat(originalTemplate, args, out _resultTemplate, out _resultArgs);
+            
+            /* Assert */
+            Assert.That(result, Is.True);
+            Assert.That(_resultTemplate, Is.EqualTo(expected));
         }
     }
 }
--- src/SerilogPreformatter.cs
+++ src/SerilogPreformatter.cs
@@ -1,6 +1,28 @@
-﻿namespace Common.Logging.Serilog
+﻿using System.Linq;
+
+namespace Common.Logging.Serilog
 {
     public class SerilogPreformatter
     {
+        public bool TryPreformat(string templateString, object[] args, out string newTemplate, out object[] newArgs)
+        {
+            if (string.IsNullOrEmpty(templateString))
+            {
+                newTemplate = templateString;
+                newArgs = args;
+                return true;
+            }
+
+            if (args == null || !args.Any())
+            {
+                newTemplate = templateString;
+                newArgs = args;
+                return true;
+            }
+
+            newTemplate = string.Format(templateString, args);
+            newArgs = null;
+            return true;
+        }
     }
 }
