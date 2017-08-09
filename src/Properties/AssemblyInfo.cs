12ea0a5b17694b8b7bccaef40c5e29922097de8d
Jaben Cargman <jaben.cargman@captiveaire.com>
2017-08-09 16:37:09 -0400

    Fixed up the broken version info.

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Common.Logging.Serilog")]
-[assembly: AssemblyCopyright("Copyright © CaptiveAire Systems 2015")]
+[assembly: AssemblyCopyright("Copyright © CaptiveAire Systems 2015-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.2.0")]
-[assembly: AssemblyFileVersion("4.2.0")]
+[assembly: AssemblyVersion("4.3.0")]
+[assembly: AssemblyFileVersion("4.3.0")]
